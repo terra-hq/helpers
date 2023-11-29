d8af0c7ff80627288afb55fefb349b8e7d688f0d
Nerea Fontecha <nerea@landt.co>
2023-11-29 12:31:56 +0100

    helpers

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,8 +1,10 @@
 import "./../scss/index.scss";
-
+// @ts-ignore
 import {getRecaptcha_v3} from './recaptcha/index.js';
 var GoogleAccesToken = await getRecaptcha_v3({
     API_KEY : "6LdJjR8pAAAAAKqofmo08imm7lsnJ7XBYj1JbC8k",
+    SECRET_KEY : "6LdJjR8pAAAAAJUK0ysqsLapCs3TfaP0N5LBR7FH",
+    action : "submit"
 });
 
 console.log(GoogleAccesToken);
--- src/js/recaptcha/index.js
+++ src/js/recaptcha/index.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 const get_file_on_demand = async (payload) => {
     return new Promise((resolve, reject) => {      
         const s = document.createElement('script');
@@ -18,13 +19,45 @@ const get_file_on_demand = async (payload) => {
     })
 }
 
+const POST_WP_TO_VALIDATE_RECAPTCHA_V3 = async (payload) => {
+    const secretKey = '6LdJjR8pAAAAAJUK0ysqsLapCs3TfaP0N5LBR7FH';
+    const userResponseToken = payload.TOKEN;
+    const siteVerifyURL = 'https://www.google.com/recaptcha/api/siteverify';
+    
+    const postData = {
+      secret: secretKey,
+      response: userResponseToken
+    };
+    
+    axios.post(siteVerifyURL, postData,  { withCredentials: true })
+      .then(response => {
+        console.log('Response:', response.data);
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error('Response error:', error.response.data);
+          console.error('Status code:', error.response.status);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error('No response received:', error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.error('Error setting up the request:', error.message);
+        }
+        console.error('Config:', error.config);
+      });
+    
+}
+
+
 const getRecaptcha_v3 = async (payload) => {
     var loadScript = await get_file_on_demand({
         API_KEY: payload.API_KEY,
     });
     if(loadScript){
         // create real promise, because execute method does not return the real one
-        return new Promise((resolve, reject) => {
+        var GoogleAccesToken = await new Promise((resolve, reject) => {
             grecaptcha.ready( () =>
                 grecaptcha.execute(payload.API_KEY, { action: payload.action })
                     .then(token => {
@@ -33,7 +66,14 @@ const getRecaptcha_v3 = async (payload) => {
                 reject)
             );  
         })
+        if(GoogleAccesToken) {
+            var response = POST_WP_TO_VALIDATE_RECAPTCHA_V3({
+                TOKEN : GoogleAccesToken,
+                SECRET_KEY : payload.SECRET_KEY
+            })   
+        }
+     
     }
-    
 }
+
 export { getRecaptcha_v3 };
