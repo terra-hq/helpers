95e6c14618098624da213d6c7cc2cc606c79aba9
Nerea Fontecha <nerea@landt.co>
2023-11-30 18:08:25 +0100

    jsutil

--- src/js/swup/index.js
+++ src/js/swup/index.js
@@ -5,10 +5,12 @@ const forceScrollToTop = ()=>{
     var jsUtil = new JSUTIL()
     return new Promise((resolve) => {
         disableScroll.on();
-        jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
-        jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
+        jsUtil.addStyle(document.querySelector("body"), "height", "100vh")
+        jsUtil.addStyle(document.querySelector("body"), "overflow", "hidden")
         setTimeout(() => {
             disableScroll.off()
+            jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
+            jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
         }, 100)
         // block scroll on page transition
         setTimeout(() => {
