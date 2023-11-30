85715663cf607348ea881220e75da1841d2e6eba
Andrés Clúa <andresclua@gmail.com>
2023-11-30 12:34:34 +0100

    update helper docs

--- src/js/hubspot/index.js
+++ src/js/hubspot/index.js
@@ -1,4 +1,37 @@
 import axios from "axios";
+
+/**
+ * Submits form data to the HubSpot API using the specified portal ID and form ID.
+ *
+ * @param {string} portalId - The HubSpot portal ID where the form is hosted.
+ * @param {string} formId - The ID of the form in the specified HubSpot portal.
+ * @param {Object} formInputs - An object containing the form field values to be submitted.
+ * @returns {Object} - An object containing the submission result.
+ * @throws {Error} - Throws an error if the submission fails.
+ *
+ * @example
+ * const portalId = 'YOUR_PORTAL_ID';
+ * const formId = 'YOUR_FORM_ID';
+ * const formInputs = {
+ *   firstName: 'John',
+ *   lastName: 'Doe',
+ *   email: 'john.doe@example.com',
+ * };
+ *
+ * try {
+ *   const submissionResult = await submitToHubspot(portalId, formId, formInputs);
+ *   console.log(submissionResult.message);
+ *   if (submissionResult.success) {
+ *     // Handle successful submission
+ *   } else {
+ *     // Handle submission error
+ *   }
+ * } catch (error) {
+ *   // Handle unexpected errors during submission
+ *   console.error('Submission error:', error.message);
+ * }
+ */
+
 const submitToHubspot = async (portalId, formId, formInputs) => {
     let success = false;
     let statusCode = 200;
--- src/js/preload/index.js
+++ src/js/preload/index.js
@@ -2,10 +2,71 @@ import imagesLoaded from "imagesloaded";
 import WebFont from "webfontloader";
 import lottieweb from "lottie-web";
 
+/**
+ * Preloads images asynchronously and resolves the Promise when all images are loaded.
+ *
+ * @param {string} [selector="img"] - Optional CSS selector for the images to preload. Default is "img".
+ * @returns {Promise} - A Promise that resolves when all images are successfully loaded.
+ *
+ * @example
+ *  Preload all images on the page
+ * preloadImages()
+ *   .then(() => {
+ *     console.log('All images preloaded successfully');
+ *     Your code to handle when images are preloaded
+ *   })
+ *   .catch((error) => {
+ *     console.error('Image preload error:', error.message);
+ *      Your error handling code
+ *   });
+ *
+ * Preload specific images using a custom selector
+ * preloadImages('.custom-images')
+ *   .then(() => {
+ *     console.log('Custom images preloaded successfully');
+ *     Your code to handle when specific images are preloaded
+ *   })
+ *   .catch((error) => {
+ *     console.error('Image preload error:', error.message);
+ *     Your error handling code
+ *   });
+ */
+
 const preloadImages = (selector = "img") =>
   new Promise((resolve) => {
     imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
-  });
+});
+
+
+/**
+ * Preloads Google Fonts asynchronously and resolves the Promise when the fonts are loaded.
+ *
+ * @param {string|string[]} id - The font family or families to preload, specified as a string or an array of strings.
+ * @returns {Promise} - A Promise that resolves when the specified Google Fonts are successfully loaded.
+ *
+ * @example
+ * Preload a single font family
+ * preloadFonts('Roboto')
+ *   .then(() => {
+ *     console.log('Roboto font preloaded successfully');
+ *    
+ *   })
+ *   .catch((error) => {
+ *     console.error('Font preload error:', error.message);
+ *    
+ *   });
+ *
+ * Preload multiple font families
+ * preloadFonts(['Montserrat', 'Open Sans'])
+ *   .then(() => {
+ *     console.log('Montserrat and Open Sans fonts preloaded successfully');
+ *    
+ *   })
+ *   .catch((error) => {
+ *     console.error('Font preload error:', error.message);
+ *
+ *   });
+ */
 
 const preloadFonts = (id) =>
   new Promise((resolve) => {
@@ -16,6 +77,25 @@ const preloadFonts = (id) =>
       active: resolve,
     });
   });
+
+/**
+ * Preloads Lottie animations asynchronously and resolves the Promise when all animations are loaded.
+ *
+ * @param {string} id - The identifier for the Lottie animations to preload.
+ * @returns {Promise} - A Promise that resolves when all Lottie animations are successfully loaded.
+ *
+ * @example
+ * // Preload Lottie animations with a specific identifier
+ * preloadLotties('my-animations')
+ *   .then(() => {
+ *     console.log('Lottie animations preloaded successfully');
+ *     // Your code to handle when the animations are preloaded
+ *   })
+ *   .catch((error) => {
+ *     console.error('Lottie animations preload error:', error.message);
+ *     // Your error handling code
+ *   });
+ */
 
 const preloadLotties = (id) =>
   new Promise(async (resolve) => {
@@ -38,6 +118,26 @@ const preloadLotties = (id) =>
     }
     resolve();
   });
+
+/**
+ * Preloads Vue.js asynchronously and resolves the Promise when Vue.js is loaded.
+ *
+ * @param {Object} payload - An object containing preload options.
+ * @param {string} payload.element - The DOM element identifier associated with Vue.js.
+ * @returns {Promise} - A Promise that resolves when Vue.js is successfully loaded.
+ *
+ * @example
+ * 
+ * preloadVue({ element: '#app' })
+ *   .then(() => {
+ *     console.log('Vue.js preloaded successfully');
+ *     
+ *   })
+ *   .catch((error) => {
+ *     console.error('Vue.js preload error:', error.message);
+ * 
+ *   });
+ */
 
 const preloadVue = (payload) =>
   new Promise((resolve) => {
