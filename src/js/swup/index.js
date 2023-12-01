76e3ecba94ff4377af37572a64ee4639faf9b446
Nerea Fontecha <nerea@landt.co>
2023-12-01 14:30:44 +0100

    version

--- src/js/swup/index.js
+++ src/js/swup/index.js
@@ -1,15 +1,12 @@
 import JSUTIL from "@andresclua/jsutil"
-import disableScroll from "disable-scroll";
 
 const forceScrollToTop = ()=>{
     var jsUtil = new JSUTIL()
     return new Promise((resolve) => {
-        disableScroll.on()
         window.scrollTo(0, 0)
         jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
         jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
         setTimeout(() => {
-            disableScroll.off()
             jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
             jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
         }, 300)
