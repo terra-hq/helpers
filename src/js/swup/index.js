24e52d738869703589bd56b385604d45c39e0e7a
Nerea Fontecha <nerea@landt.co>
2023-12-01 12:20:03 +0100

    scrolltotop

--- src/js/swup/index.js
+++ src/js/swup/index.js
@@ -4,20 +4,18 @@ import JSUTIL from "@andresclua/jsutil"
 const forceScrollToTop = ()=>{
     var jsUtil = new JSUTIL()
     return new Promise((resolve) => {
+        disableScroll.on()
+        window.scrollTo(0, 0)
+        jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
+        jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
         setTimeout(() => {
-            disableScroll.on()
-            window.scrollTo(0, 0)
+            disableScroll.off()
             jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
             jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
-            setTimeout(() => {
-                disableScroll.off()
-                jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
-                jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
-            }, 300)
-            setTimeout(() => {
-                resolve()
-            }, 500)
-        }, 10)
+        }, 300)
+        setTimeout(() => {
+            resolve()
+        }, 500)
     })
 }
 
