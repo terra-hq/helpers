caa9d9aa5670a6375800ddf381d56ca1910b1ab7
Nerea Fontecha <nerea@landt.co>
2023-12-01 10:58:41 +0100

    scroll

--- src/js/swup/index.js
+++ src/js/swup/index.js
@@ -4,18 +4,20 @@ import JSUTIL from "@andresclua/jsutil"
 const forceScrollToTop = ()=>{
     var jsUtil = new JSUTIL()
     return new Promise((resolve) => {
-        disableScroll.on();
-        jsUtil.addStyle(document.querySelector("body"), "height", "100vh")
-        jsUtil.addStyle(document.querySelector("body"), "overflow", "hidden")
+        setTimeout(() => {
+            disableScroll.on()
+            window.scrollTo(0, 0)
+            jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
+            jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
+        }, 10)
         setTimeout(() => {
             disableScroll.off()
             jsUtil.addStyle(document.querySelector("body"), 'height', 'auto')
             jsUtil.addStyle(document.querySelector("body"), 'overflow', 'initial')
-        }, 100)
-        // block scroll on page transition
+        }, 300)
         setTimeout(() => {
             resolve()
-        }, 500)
+        }, 1000)
     })
 }
 
