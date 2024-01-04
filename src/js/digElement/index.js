f2ff52a7ae2d5d02ed0ae4dcb4f5bd50a73340c5
guidogambini1995 <132886578+guidogambini1995@users.noreply.github.com>
2024-01-04 17:50:22 +0100

    add params

--- src/js/digElement/index.js
+++ src/js/digElement/index.js
@@ -1,5 +1,34 @@
 import JSUTIL from "@andresclua/jsutil";
 
+/**
+ * Inspect a specified HTML element for mutations in its styles, classes, data attributes, or the structure of its children.
+ * @param {Object} payload - The payload containing different arguments.
+ * @param {HTMLElement} payload.element - The element to check.
+ * @param {Object} payload.search - It includes some options.
+ * @param {String} payload.search.type - Define if you are looking for style, class or children elements.
+ * @param {Array} payload.search.lookFor - Array of style properties/classes/data-attribute values.
+ * @param {String} payload.search.attribute - The data attribute you are looking for.
+ * @returns {Promise<boolean>} A Promise that resolves to `true` when the requested values are matched.
+ *
+ * @example
+ * // Example usage:
+ * const intervalId = setInterval(() => {
+    (async () => {
+        const readyElement = await digElement({
+            element: element,
+            search: {
+                type: "style",
+                lookFor: ["max-height", "visibility"],
+            },
+        });
+
+        if (readyElement) {
+            clearInterval(intervalId);
+        }
+    })();
+}, 1000);
+ */
+
 const digElement = async (payload) => {
     if (!payload.element) return Promise.resolve(false);
 
