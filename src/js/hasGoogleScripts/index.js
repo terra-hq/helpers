dcb38c2b61d4ee99f859ad2742905e74345e28b5
Alejo Schlegel <alejo@terrahq.com>
2024-11-04 10:55:17 +0100

    fix: :bug: hasGoogleScripts config debug false by default

--- src/js/hasGoogleScripts/index.js
+++ src/js/hasGoogleScripts/index.js
@@ -1,13 +1,14 @@
 /**
  * Checks for the presence of specific Google scripts on the page, such as Google Analytics and Google Tag Manager.
  * The function uses a timeout mechanism to repeatedly check for these scripts and resolves a promise based on their detection.
+ * A debug option can be enabled to log information that match the selector.
  * 
  * @param {Object} [options] - Configuration options for the script detection.
  * @param {Array<string>} [options.detect=['analytics', 'gtm']] - Array of Google scripts to check for. Possible values are:
  *    - `'analytics'`: Checks for Google Analytics cookies.
  *    - `'gtm'`: Checks for the presence of Google Tag Manager's dataLayer object.
  * @param {number} [options.maxTime=5000] - Maximum time in milliseconds to wait for script detection before resolving the promise.
- * @param {boolean} [options.enableLogging=true] - Flag to enable or disable console logging for debugging purposes.
+ * @param {boolean} [options.debug=false] - Optional debug flag to log information about matched images. Default is false.
  * @returns {Promise<boolean>} - A promise that resolves to `true` if the specified Google scripts are detected, and `false` if not.
  *
  * @example
@@ -15,7 +16,7 @@
  * hasGoogleScripts({
  *   detect: ['analytics', 'gtm'],
  *   maxTime: 5000,
- *   enableLogging: true
+ *   debug: true
  * })
  * .then((detected) => {
  *   if (detected) {
@@ -29,11 +30,11 @@
  * });
  */
 
-export function hasGoogleScripts(options = { detect: ['analytics', 'gtm'], maxTime: 5000, enableLogging: true }) {
+export function hasGoogleScripts(options = { detect: ['analytics', 'gtm'], maxTime: 5000, debug: false }) {
     return new Promise((resolve, reject) => {
         const startTime = Date.now();
         const detectOptions = options.detect || ['analytics', 'gtm'];
-        const enableLogging = options.enableLogging !== undefined ? options.enableLogging : true;
+        const debug = options.debug !== undefined ? options.debug : false;
 
         function checkForUniversalAnalytics() {
             const hasGACookies = document.cookie.split(';').some(cookie => cookie.trim().startsWith('_ga'));
@@ -55,17 +56,17 @@ export function hasGoogleScripts(options = { detect: ['analytics', 'gtm'], maxTi
             const isAdsDetected = detectOptions.includes('ads') ? isGoogleAdsInstalled() : true;
 
             if (isAnalyticsDetected && isGTMDetected && isAdsDetected) {
-                if (enableLogging) {
+                if (debug) {
                     console.log('Specified Google scripts are detected.');
                 }
                 resolve(true);
             } else if (currentTime - startTime > options.maxTime) {
-                if (enableLogging) {
+                if (debug) {
                     console.log('Maximum time exceeded. Resolving promise.');
                 }
                 resolve(false);
             } else {
-                if (enableLogging) {
+                if (debug) {
                     console.log('Checking for specified Google scripts...');
                 }
                 setTimeout(checkScripts, 1000);
