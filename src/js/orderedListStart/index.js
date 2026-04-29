cf77ecf0c3fd2f31c2be4148716439d00980578e
Amaia del Olmo <amaia@longneckandthunderfoot.com>
2026-04-29 12:38:30 +0200

    fix: 🐛 Add ordered lists to exports

--- src/js/orderedListStart/index.js
+++ src/js/orderedListStart/index.js
@@ -4,9 +4,9 @@
  * Ordered lists with a start attribute will have their counter-reset CSS property
  * set to the value of the start attribute, minus 1. This is because the counter
  * starts at 1, and the start attribute is 1-indexed.
- * 
+ *
  * @function orderedListStart
- * @param {HTMLObject} list - The ordered list element or an array of ordered list elements.
+ * @param {Element|NodeList|HTMLCollection|Element[]} list - The ordered list element, or a collection of ordered list elements.
  * @example
  * <ol id="my-list" start="4">
  *     <li>Item 4</li>
@@ -15,27 +15,23 @@
  * </ol>
  *
  * The above code will be rendered as:
- * <ol id="my-list" style="counter-reset: list-item 3;">
+ * <ol id="my-list" start="4" style="counter-reset: list-item 3;">
  *     <li>4. Item 4</li>
  *     <li>5. Item 5</li>
  *     <li>6. Item 6</li>
  * </ol>
- *
- * The start attribute is removed from the element after the counter-reset style is set.
  */
 export function orderedListStart(list) {
-    const startLists = list instanceof Element ? [list] : list;
+    if (!list) return;
+    const startLists = list instanceof Element ? [list] : Array.from(list);
 
-    if (startLists.length > 0) {
-        startLists.forEach((ol) => {
-            const startAttr = ol.getAttribute('start');
-            if (startAttr !== null) {
-                const start = parseInt(startAttr, 10);
-                if (!isNaN(start)) {
-                    ol.style.counterReset = 'list-item ' + (start - 1);
-                    ol.removeAttribute('start');
-                }
+    startLists.forEach((ol) => {
+        const startAttr = ol.getAttribute('start');
+        if (startAttr !== null) {
+            const start = parseInt(startAttr, 10);
+            if (!isNaN(start)) {
+                ol.style.counterReset = 'list-item ' + (start - 1);
             }
-        })
-    }
+        }
+    });
 }
